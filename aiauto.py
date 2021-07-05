a40207bbecee1b7f467b6491daad63bf234ee6bf
thegreatss <86958263+thegreatss@users.noreply.github.com>
2021-07-05 19:13:22 +0900

    Update aiauto.py

--- aiauto.py
+++ aiauto.py
@@ -6,6 +6,14 @@ from fbprophet import Prophet
 
 access = "your-access"
 secret = "your-secret"
+myToken = "xoxb-your-token"
+
+def post_message(token, channel, text):
+    """슬랙 메시지 전송"""
+    response = requests.post("https://slack.com/api/chat.postMessage",
+        headers={"Authorization": "Bearer "+token},
+        data={"channel": channel,"text": text}
+    )
 
 def get_target_price(ticker, k):
     """변동성 돌파 전략으로 매수 목표가 조회"""
@@ -58,6 +66,8 @@ schedule.every().hour.do(lambda: predict_price("KRW-BTC"))
 # 로그인
 upbit = pyupbit.Upbit(access, secret)
 print("autotrade start")
+# 시작 메세지 슬랙 전송
+post_message(myToken,"#crypto", "autotrade start")
 
 # 자동매매 시작
 while True:
@@ -73,12 +83,15 @@ while True:
             if target_price < current_price and current_price < predicted_close_price:
                 krw = get_balance("KRW")
                 if krw > 5000:
-                    upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    buy_result = upbit.buy_market_order("KRW-BTC", krw*0.9995)
+                    post_message(myToken,"#crypto", "BTC buy : " +str(buy_result))
         else:
             btc = get_balance("BTC")
             if btc > 0.00008:
-                upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                sell_result = upbit.sell_market_order("KRW-BTC", btc*0.9995)
+                post_message(myToken,"#crypto", "BTC buy : " +str(sell_result))
         time.sleep(1)
     except Exception as e:
         print(e)
+        post_message(myToken,"#crypto", e)
         time.sleep(1)
